e228c0b2618aecf107a732223955441bb37e3690
larry barrera <larrybarrera0066@gmail.com>
2026-02-06 11:25:14 -0800

    feb 4 video

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -2,23 +2,62 @@
 //  ContentView.swift
 //  DungeonDice
 //
-//  Created by BARRERA, LARRY on 2/6/26.
+//  Created by BARRERA, LARRY on 2/4/26.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    enum Dice: Int, CaseIterable {
+        case four = 4
+        case six = 6
+        case eight = 8
+        case ten = 10
+        case twelve = 12
+        case twenty = 20
+        case hundred = 100
+        
+        func roll() -> Int {
+            return Int.random(in: 1...self.rawValue)
+        }
+    }
+    
+    @State private var resultMessage = ""
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dungeon Dice")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(.red)
+            
+            Spacer()
+            
+            Text(resultMessage)
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+                .fontWeight(.medium)
+                .frame(height: 150)
+            
+            Spacer()
+            
+
+                ForEach(Dice.allCases, id: \.self) { dice in
+                    Button("\(dice.rawValue)-sided") {
+                        resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                    }
+                }
+            .buttonStyle(.borderedProminent)
+            .tint(.red)
+
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider{
+    static var previews: some View{
+        ContentView()
+    }
 }
