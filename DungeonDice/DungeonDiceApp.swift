cb77782a557edba8bf6917a93474e69801c4f240
larry barrera <larrybarrera0066@gmail.com>
2026-02-06 10:31:22 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by BARRERA, LARRY on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
